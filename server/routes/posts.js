3a0a4dff94115ba2028513b4f5fd6ad64425d44b
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-05-28 09:47:42 +1200

    Add comments in posts routes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -8,6 +8,7 @@ const router = express.Router()
 router.get('/', (req, res) => {
   db.getPosts()
     .then(posts => {
+      // render all posts in JSON
       res.json(posts)
     })
 })
@@ -16,19 +17,24 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   const { id } = req.params
 
+  // call getPostById by using Id from params
   db.getPostById(id)
     .then(post => {
+      // render singular post returned
       res.json(post)
     })
 })
 
 // POST /v1/posts
 router.post('/', (req, res) => {
+  // define what is needed for a newPost (what user enters)
   const newPost = {
     title: req.body.title,
     paragraphs: req.body.paragraphs
   }
+
   db.addPost(newPost)
+  // use id given from newPost to find the single post again
     .then(id => db.getPostById(id))
     .then(post => {
       res.json(post)
