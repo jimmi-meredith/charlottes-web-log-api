f3a6b430d7b83760c159d1b2cc0bb5b0f5fb1530
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-05-28 09:41:31 +1200

    Add comments in db.js

--- server/db/db.js
+++ server/db/db.js
@@ -9,17 +9,22 @@ module.exports = {
 }
 
 function getPosts () {
+  // returning all posts in Posts table
   return db('Posts')
 }
 
 function getPostById (id) {
   return db('Posts')
+  // only returns posts which have an id that matches the id in the params (defined in routes)
     .where('id', id)
+    // selects the first post that matches so it is no longer one post in an array
     .first()
 }
 
 function addPost (post) {
   return db('Posts')
+  // adds the new post to the Posts table
     .insert(post)
+    // GET CLARIFICATION HERE!!!
     .then(([id]) => id)
 }
