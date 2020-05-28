30c03e8557a6f266649516eddb17c8ee242f4b48
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-05-28 15:04:32 +1200

    Move routes back to posts, fixing issue created from earlier move...

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -4,28 +4,4 @@ const db = require('../db/db')
 
 const router = express.Router()
 
-// GET /v1/posts/:postId/comments
-router.get('/:postId/comments', (req, res) => {
-  const { postId } = req.params
-  // retreives the comments that match a posts id
-  db.getPostComments(postId)
-    .then(comments => {
-      res.json(comments)
-    })
-})
-
-// POST /v1/posts/:postId/comments
-router.post('/:postId/comments', (req, res) => {
-  const { postId } = req.params
-  const comment = {
-    post_id: postId,
-    comment: req.body.comment
-  }
-
-  db.addPostComment(comment)
-    .then(id => {
-      res.json(id)
-    })
-})
-
 module.exports = router
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -68,4 +68,28 @@ router.delete('/:id', (req, res) => {
       res.json(posts))
 })
 
+// GET /v1/posts/:postId/comments
+router.get('/:postId/comments', (req, res) => {
+  const { postId } = req.params
+  // retreives the comments that match a posts id
+  db.getPostComments(postId)
+    .then(comments => {
+      res.json(comments)
+    })
+})
+
+// POST /v1/posts/:postId/comments
+router.post('/:postId/comments', (req, res) => {
+  const { postId } = req.params
+  const comment = {
+    post_id: postId,
+    comment: req.body.comment
+  }
+
+  db.addPostComment(comment)
+    .then(id => {
+      res.json(id)
+    })
+})
+
 module.exports = router
