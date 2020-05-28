25a023ac29b2714fc1819a3ff1610cf4a2f70d3b
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-05-28 17:02:56 +1200

    fix error in updateComment route

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -7,7 +7,9 @@ const router = express.Router()
 // PUT /v1/comments/:commentId
 router.put('/:commentId', (req, res) => {
   const { commentId } = req.params
-  const { comment } = req.body
+  const comment = {
+    comment: req.body.comment
+  }
 
   db.updateComment(commentId, comment)
     .then(() => db.getCommentById(commentId))
