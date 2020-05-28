7a3fd4cba25e4faf21fe6b06a56a1e0d56737592
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-05-28 17:00:58 +1200

    Write updateComment put route

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -4,13 +4,16 @@ const db = require('../db/db')
 
 const router = express.Router()
 
-// // PUT /v1/comments/:commentId
-// router.put('/:commentId', (req, res) => {
-//   const { commentId } = req.params
-//   const { comment } = req.body
+// PUT /v1/comments/:commentId
+router.put('/:commentId', (req, res) => {
+  const { commentId } = req.params
+  const { comment } = req.body
 
-//   db.updateComment(commentId, comment)
-//     .then(() => db.get
-// })
+  db.updateComment(commentId, comment)
+    .then(() => db.getCommentById(commentId))
+    .then(comment => {
+      res.json(comment)
+    })
+})
 
 module.exports = router
